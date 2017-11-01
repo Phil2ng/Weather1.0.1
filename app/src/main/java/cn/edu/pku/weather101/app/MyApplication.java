d7691c027df53e66a07ddb733143c287ff6a9853
王飞 <f1570053668@163.com>
2017-11-01 17:07:37 +0800

    从数据库中读取城市列表

---
+++ app/src/main/java/cn/edu/pku/weather101/app/MyApplication.java
@@ -0,0 +1,104 @@
+package cn.edu.pku.weather101.app;
+
+import android.app.Application;
+import android.os.Environment;
+import android.util.Log;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import cn.edu.pku.weather101.bean.City;
+import cn.edu.pku.weather101.db.CityDB;
+
+public class MyApplication extends Application {
+    private static final String TAG = "MyAPP";
+    private static MyApplication mApplication;
+    private CityDB mCityDB;
+    private List<City> mCityList;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d(TAG, "MyApplication->onCreate");
+
+        mApplication = this;
+        mCityDB = openCityDB();
+        initCityList();
+    }
+
+    public static MyApplication getInstance() {
+        return mApplication;
+    }
+
+    private CityDB openCityDB() {
+        String path = "/data"
+                + Environment.getDataDirectory().getAbsolutePath()
+                + File.separator + getPackageName()
+                + File.separator + "databases"
+                + File.separator + CityDB.CITY_DB_NAME;
+        File db = new File(path);
+        Log.d(TAG, path);
+        if (!db.exists()) {
+            String pathfolder = "/data"
+                    + Environment.getDataDirectory().getAbsolutePath()
+                    + File.separator + getPackageName()
+                    + File.separator + "databases"
+                    + File.separator;
+
+            File dirFirstFolder = new File(pathfolder);
+            if (!dirFirstFolder.exists()) {
+                dirFirstFolder.mkdirs();
+                Log.i("MyApp", "mkdirs");
+            }
+            Log.i("MyApp", "db is not exists");
+            try {
+                InputStream is = getAssets().open("city.db");
+                FileOutputStream fos = new FileOutputStream(db);
+                int len = -1;
+                byte[] buffer = new byte[1024];
+                while ((len = is.read(buffer)) != -1) {
+                    fos.write(buffer, 0, len);
+                    fos.flush();
+                }
+                fos.close();
+                is.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+                System.exit(0);
+            }
+        }
+        return new CityDB(this, path);
+    }
+
+    private void initCityList() {
+        mCityList = new ArrayList<City>();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+    // TODO Auto-generated method stub
+                prepareCityList();
+            }
+        }).start();
+    }
+
+    private boolean prepareCityList() {
+        mCityList = mCityDB.getAllCity();
+        int i = 0;
+        for (City city : mCityList) {
+            i++;
+            String cityName = city.getCity();
+            String cityCode = city.getNumber();
+            Log.d(TAG, cityCode + ":" + cityName);
+        }
+        Log.d(TAG, "i=" + i);
+        return true;
+    }
+
+    public List<City> getCityList() {
+        return mCityList;
+    }
+}
